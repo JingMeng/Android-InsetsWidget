0c5dd95b9a870373baa350de7e552ac71fa4c602
TakWolf <takwolf@foxmail.com>
2022-02-06 08:09:26 +0800

    完成 Demo

---
+++ app/src/main/java/com/takwolf/android/demo/insetswidget/Demo1Activity.kt
@@ -0,0 +1,13 @@
+package com.takwolf.android.demo.insetswidget
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+
+class Demo1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_demo_1)
+    }
+}
--- app/src/main/java/com/takwolf/android/demo/insetswidget/MainActivity.kt
+++ app/src/main/java/com/takwolf/android/demo/insetswidget/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.takwolf.android.demo.insetswidget
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.takwolf.android.demo.insetswidget.databinding.ActivityMainBinding
@@ -10,8 +11,16 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnDemo.setOnClickListener {
-            // TODO
+        binding.btnDemo1.setOnClickListener {
+            startActivity(Intent(this, Demo1Activity::class.java))
+        }
+
+        binding.btnDemo2.setOnClickListener {
+            startActivity(Intent(this, Demo2Activity::class.java))
+        }
+
+        binding.btnDemo3.setOnClickListener {
+            startActivity(Intent(this, Demo3Activity::class.java))
         }
     }
 }
